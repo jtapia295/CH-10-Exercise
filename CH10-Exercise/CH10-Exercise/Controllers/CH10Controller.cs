d9d960b0f66580a12355631da929ed412ae272e7
jtapia295 <85265752+jtapia295@users.noreply.github.com>
2021-07-19 16:10:05 -0400

    Complete CH10 Tut

---
+++ CH10-Exercise/CH10-Exercise/Controllers/CH10Controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CH10_Exercise.Controllers
+{
+    [Route("/CH10")]
+    public class CH10Controller : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html =$"{"<form method='post' action='/CH10'>"}{"<input type = 'text' name = 'name'/>"}{"<input type = 'Submit' Value = 'Greet Me!'/>"}{"</form> "}";
+            return Content(html,"text/html");
+        }
+
+        //Get /hello/welcome/{string parameter"}
+        [HttpGet("welcome/{name?}")]
+        [HttpPost]
+        public IActionResult Welcome(string name = "World")
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+    }
+}
---
+++ CH10-Exercise/CH10-Exercise/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CH10_Exercise.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
