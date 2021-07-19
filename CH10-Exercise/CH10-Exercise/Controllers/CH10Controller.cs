64470b5a4da2908e22b062be17488554f9fb034e
jtapia295 <85265752+jtapia295@users.noreply.github.com>
2021-07-19 18:21:54 -0400

    Completed CH10 Exercise

--- CH10-Exercise/CH10-Exercise/Controllers/CH10Controller.cs
+++ CH10-Exercise/CH10-Exercise/Controllers/CH10Controller.cs
@@ -15,16 +15,39 @@ namespace CH10_Exercise.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            string html =$"{"<form method='post' action='/CH10'>"}{"<input type = 'text' name = 'name'/>"}{"<input type = 'Submit' Value = 'Greet Me!'/>"}{"</form> "}";
-            return Content(html,"text/html");
+            string html = $"{"<form method='post' action='/CH10/hello'>"}{"<input type = 'text' name = 'name'/>"}{"<select name = 'language'><option value='English'>English</option><option value='Espanol'>Espanol</option><option value='Italiano'>Italiano</option><option value='Irish'>Irish</option><option value='Portuguese'>Portuguese</option>" }{"<input type = 'Submit' Value = 'Greet Me!'/>"}{"</form> "}";
+            return Content(html, "text/html");
         }
 
         //Get /hello/welcome/{string parameter"}
-        [HttpGet("welcome/{name?}")]
+        //[HttpGet("welcome/{name?}")]
+        //[HttpPost]
+        //public IActionResult Welcome(string name = "World")
+        //{
+
+        //    return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        //}
+
+        [HttpGet]
         [HttpPost]
-        public IActionResult Welcome(string name = "World")
+        [Route("hello")]
+        public IActionResult CreateMessage(string name, string language)
         {
-            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+            switch (language)
+            {
+                case "English":
+                    return Content("<h1>Hello, " + name + "!</h1>", "text/html");
+                case "Espanol":
+                    return Content("<h1>Hola, " + name + "!</h1>", "text/html");
+                case "Italiano":
+                    return Content("<h1>Ciao, " + name + "!</h1>", "text/html");
+                case "Irish":
+                    return Content("<h1>Dia dhuit, " + name + "!</h1>", "text/html");
+                case "Portuguese":
+                    return Content("<h1>Ola, " + name + "!</h1>", "text/html");
+                default:
+                    return Content("<h1>Hello, " + name + "!</h1>", "text/html");
+            }
         }
     }
 }
